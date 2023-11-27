cf6937875968eacba2d6de7fa0c9c1cf2dd3be4e
RunWang0402 <mwwpositive1000@gmail.com>
2023-11-27 13:03:17 -0800

    changed grade.sh

--- grade.sh
+++ grade.sh
@@ -34,7 +34,12 @@ cp lib/junit-4.13.2.jar grading-area/
 
 cd grading-area
 
-javac -cp .:hamcrest-core-1.3.jar:junit-4.13.2.jar *.java
+javac -cp .:hamcrest-core-1.3.jar:junit-4.13.2.jar *.java 2>compile.txt
+
+if grep -q "error" "compile.txt"; then 
+    cat compile.txt
+    exit 1
+fi
 
 if [[ $? -ne 0 ]]; then
     echo Cannot compile java files
@@ -43,16 +48,23 @@ fi
 
 java -cp .:hamcrest-core-1.3.jar:junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > output.txt
 
+if grep -q "OK" "output.txt"; then
+    echo Grade 100
+    exit 0
+fi
+
 numberRun=$(grep -o 'run: [0-9]*' output.txt | grep -o '[0-9]*')
+
 
 failure=$(grep -o 'Failures: [0-9]*' output.txt | grep -o '[0-9]*')
 
+
 grep 'Tests run' output.txt
 
-grade=$(($numberRun - $failure))
+grade=$(($numberRun-$failure))
 
 echo $grade
-echo $failure
+#echo $failure
 
 #grep -o 'run: [0-9]*' output.txt
 
